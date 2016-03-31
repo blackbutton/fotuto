c870a36bf8d4f8b2e0d38cbbb78e55a9a4f83972
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-03-30 21:31:12 -0500

    SPA: e2e tests refactoring

--- static/spa/e2e/main-test.js
+++ static/spa/e2e/main-test.js
@@ -1,67 +1,117 @@
-describe('Given I am access to the site', function () {
+describe("Fotuto App", function () {
+	var base_url = 'static/spa/index.html';
 
-	//describe("Fotuto shoulds appears in page title", function () {
-	beforeEach(function () {
-		// Assemble
-		browser.get('static/spa/index.html');
+	// TODO: Add alternative flows
+
+	describe("Given I am access to the Fotuto site", function () {
+		beforeEach(function () {
+			// Assemble
+			browser.get(base_url);
+		});
+
+		it("Should display Fotuto in page title", function () {
+			expect(browser.getTitle()).toMatch(/Fotuto.*$/);
+		});
+
+		// TODO: display login form first
+		//Assert
+		// Since there is no any windows, the configuration wizard intro is shown
+		it("Should redirect index.html to index.html#/config/intro", function () {
+			browser.getLocationAbsUrl().then(function (url) {
+				expect(url).toEqual('/config/intro');
+			});
+		});
 	});
 
-	//Assert
-	it('The page title should display Fotuto', function () {
-		expect(browser.getTitle()).toEqual('Fotuto');
+	describe("Given I am access to the configuration wizard", function () {
+		beforeEach(function () {
+			// TODO: Login as an operator
+			// Assemble
+			browser.get(base_url + '#/config/intro');
+		});
+
+		// TODO: Logged in user should be an operator to access to configuration
+		it("Should display configuration wizard intro page", function () {
+			// Config Wizard Intro page
+			var wizard_step_view = element(by.css('#ConfigWizardIntroView'));
+			expect(wizard_step_view.isPresent()).toBeTruthy();
+
+			// TODO: Should display Configuration in page title
+			// expect(browser.getTitle()).toMatch(/Fotuto - Configuration$/);
+
+			// Check page title as Configuration Wizard
+			var page_heading = element.all(by.tagName('h2')).first();
+			// TODO: If the logged in user not have enough permissions instead config wizard it should display "No windows configured" message
+			expect(page_heading.getText()).toBe('Configuration Wizard');
+		});
+
+		it("Should I allow to go inside the wizard", function () {
+			// Click start wizard button
+			var start_wizard_button = element.all(by.linkText("Start now!"));
+			expect(start_wizard_button.count()).toBe(1);
+			start_wizard_button.click();
+
+			// Now I'm on the wizard step 1
+			var expected_url = '/config/steps/1';
+			browser.getLocationAbsUrl().then(function (url) {
+				expect(url).toEqual(expected_url);
+			});
+		});
 	});
-	//});
 
-	// TODO: It should display the login page
+	// Start wizard
+	// Now user is on wizard step 1
+	describe("Given I am started the first step of the configuration wizard", function () {
+		beforeEach(function () {
+			// TODO: Login as an operator
+			// Assemble
+			browser.get(base_url + '#/config/steps/1');
+		});
 
-	it("Since there is no any windows, the configuration wizard intro is shown", function () {
+		// TODO: Logged in user should be an operator to access to configuration
+		it("Should display configuration wizard step 1 page", function () {
+			// Config Wizard Device page
+			var view_wizard_step = element(by.css('#ConfigWizardStepDevicesView'));
+			expect(view_wizard_step.isPresent()).toBeTruthy();
 
-		// Config Wizard Intro page
-		var wizard_step_view = element(by.css('#ConfigWizardIntroView'));
-		expect(wizard_step_view.isPresent()).toBeTruthy();
+			// TODO: Notice the step navigator
+			// TODO: Wizard navigator only have step 1 active and other steps deactivated
 
-		// Check page title as Configuration Wizard
-		var page_heading = element.all(by.tagName('h2')).first();
-		// TODO: If the logged in user not have enough permissions instead config wizard it should display "No windows configured" message
-		expect(page_heading.getText()).toBe('Configuration Wizard');
+			// Notice the Devices subtitle
+			var page_subheading = element.all(by.tagName('h3')).first();
+			expect(page_subheading.getText()).toBe('Devices');
 
-		// Click start wizard button
-		var start_wizard_button = element.all(by.linkText("Start now!"));
-		expect(start_wizard_button.count()).toBe(1);
-		start_wizard_button.click();
+			// Notice there is an empty list of devices
+			var list_devices = element.all(by.repeater('device in devices'));
+			expect(list_devices.count()).toBe(0);
 
-		// Start wizard
-		// Now user is on wizard step 1
-		// Config Wizard Intro page
-		var view_wizard_step = element(by.css('#ConfigWizardStepDevicesView'));
-		expect(view_wizard_step.isPresent()).toBeTruthy();
+			// TODO: Notice Device form
+			// TODO: Notice Next step button (disabled since there is no device)
+		});
 
-		// Notice there is an empty list of device
-		var page_subheading = element.all(by.tagName('h3')).first();
-		expect(page_subheading.getText()).toBe('Devices');
-		// TODO: Notice the step navigator
-		// Wizard navigator only have step 1 active and other steps deactivated
-		// Notice the Devices subtitle
-		var list_devices = element.all(by.repeater('device in devices'));
-		expect(list_devices.count()).toBe(0);
+		it("Should allow me to add devices", function () {
+			// Enter new device data
+			// Device fields
+			$("input[name='name']").sendKeys('Alarm Controller');
+			// TODO: Slug field should generated automatically and it should not appear here
+			$("input[name='slug']").sendKeys('alarm-controller');
+			$("input[name='address']").sendKeys('0001');
 
-		// Notice Device form
-		// Enter new device data
-		// Device fields
-		$("input[name='name']").sendKeys('Alarm Controller');
-		// TODO: Slug field should generated automatically and it should not appear here
-		$("input[name='slug']").sendKeys('alarm-controller');
-		$("input[name='address']").sendKeys('0001');
+			// Submit form
+			var add_button = element(by.buttonText('Add'));
+			add_button.click();
 
-		// Submit form
-		var add_button = element(by.buttonText('Add'));
-		add_button.click();
+			// Device now appears on the list
+			var list_devices = element.all(by.repeater('device in devices'));
+			expect(list_devices.count()).toBe(1);
+			expect(list_devices.first().getText()).toBe('Alarm Controller');
 
-		// Device now appears on the list
-		expect(list_devices.count()).toBe(1);
-		expect(list_devices.first().getText()).toBe('Alarm Controller');
-
-		// Form is cleared
+			// TODO: Form is cleared
+			// TODO: Add another device
+			// TODO: Allow to inactive devices
+			// TODO: Click on Next step button
+			// TODO: Now show the wizard step 2
+		});
 	});
 
 	/* TODO: 	it("Add two vars to the device", function () {
@@ -93,5 +143,4 @@ describe('Given I am access to the site', function () {
 
 	 });*/
 
-})
-;
+});
