db6e401466376251ea0bf5faabd62799eed178a7
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-03-31 00:16:54 -0500

    SPA: minor improvement in file name and todo indications

--- static/spa/app/fotuto-app.js
+++ static/spa/app/fotuto-app.js
@@ -1,7 +1,9 @@
 var fotuto = angular.module('fotuto', ['ngRoute']);
 
+// TODO: Refactor urls by components
 fotuto.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
+	// Component: config-wizard
 		.when('/config/intro', {
 			templateUrl: 'components/config-wizard/intro.html'
 		})
