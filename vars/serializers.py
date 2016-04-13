44839807ed6d528b9d75390fd1ee3b1028213c0f
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-04-13 00:29:02 -0500

    SPA: Auto-generate slug on config devices

--- vars/serializers.py
+++ vars/serializers.py
@@ -13,7 +13,13 @@ class DeviceSerializer(serializers.ModelSerializer):
         read_only_fields = ('slug',)
 
     def validate(self, data):
-        data['slug'] = slugify(data['name'])
+        # TODO: This logic should reside in model save method
+        name_slug = slugify(data['name'])
+        data['slug'] = name_slug
+        i = 1
+        while Device.objects.filter(slug=data['slug']).count() > 0:
+            data['slug'] = '{}-{}'.format(name_slug, i)
+            i += 1
         return data
 
     def get_links(self, obj):
