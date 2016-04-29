66174ee4a83caa2cc8cc54eaa88b215cc9398a57
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-04-29 09:36:35 -0500

    Display new mimics models in admin

--- mimics/admin.py
+++ mimics/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from mimics.models import Mimic
+from mimics.models import Mimic, MimicVar, Rule
 
 admin.site.register(Mimic)
+admin.site.register(MimicVar)
+admin.site.register(Rule)
