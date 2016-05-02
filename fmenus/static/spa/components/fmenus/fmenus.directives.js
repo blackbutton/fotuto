aef3a3ecafeca41cd4d47516c68f6b08a324da6d
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-02 16:14:42 -0500

    SPA: Add menu directive

---
+++ fmenus/static/spa/components/fmenus/fmenus.directives.js
@@ -0,0 +1,12 @@
+fotuto.directive("fMenu", function () {
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: '/static/spa/components/fmenus/menu.html',
+		controller: ['$scope', '$http', '$attrs', function ($scope, $http, $attrs) {
+			$http.get('/api/menus/?position=' + $attrs.position).success(function (data) {
+				$scope.menus = {'view': data.results}
+			})
+		}]
+	}
+});
