0a4d72f64401314330c5280a485bf04025a6f05e
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-05 02:14:44 -0500

    SPA: Display submenus correctly

--- fmenus/static/spa/components/fmenus/fmenus.directives.js
+++ fmenus/static/spa/components/fmenus/fmenus.directives.js
@@ -6,7 +6,14 @@ fotuto.directive("fMenu", function () {
 		controller: ['$scope', '$http', '$attrs', function ($scope, $http, $attrs) {
 			$http.get('/api/menus/?position=' + $attrs.position).success(function (data) {
 				$scope.menus = data.results
-			})
+			});
+
+			$scope.open_menu = function (has_submenu, $mdOpenMenu, $event) {
+				if (has_submenu) {
+					$event.preventDefault();
+					$mdOpenMenu($event)
+				}
+			}
 		}]
 	}
 });
