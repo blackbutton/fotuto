4498f6edad873cc436cc29494d8472725af04018
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-04 23:30:25 -0500

    SPA: Remove unnecessary attribute view in menu

--- fmenus/static/spa/components/fmenus/fmenus.directives.js
+++ fmenus/static/spa/components/fmenus/fmenus.directives.js
@@ -5,7 +5,7 @@ fotuto.directive("fMenu", function () {
 		templateUrl: '/static/spa/components/fmenus/menu.html',
 		controller: ['$scope', '$http', '$attrs', function ($scope, $http, $attrs) {
 			$http.get('/api/menus/?position=' + $attrs.position).success(function (data) {
-				$scope.menus = {'view': data.results}
+				$scope.menus = data.results
 			})
 		}]
 	}
