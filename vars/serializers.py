68869e78f2ba3e5a5a0ff8bf2b6138b3e0f2dc68
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-09 11:22:02 -0500

    API: Display full var fields on device vars

--- vars/serializers.py
+++ vars/serializers.py
@@ -4,32 +4,6 @@ from rest_framework.reverse import reverse as drf_reverse
 from .models import Device, Var
 
 
-class DeviceSerializer(serializers.ModelSerializer):
-    vars = serializers.HyperlinkedRelatedField(view_name='var-detail', read_only=True, many=True)
-    links = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Device
-        read_only_fields = ('slug',)
-
-    def validate(self, data):
-        # TODO: This logic should reside in model save method
-        name_slug = slugify(data['name'])
-        data['slug'] = name_slug
-        i = 1
-        while Device.objects.filter(slug=data['slug']).count() > 0:
-            data['slug'] = '{}-{}'.format(name_slug, i)
-            i += 1
-        return data
-
-    def get_links(self, obj):
-        request = self.context['request']
-        return {
-            'self': drf_reverse('device-detail', kwargs={'pk': obj.pk}, request=request),
-            'vars': drf_reverse('var-list', request=request) + '?device={}'.format(obj.pk),
-        }
-
-
 class VarSerializer(serializers.ModelSerializer):
     var_type_display = serializers.SerializerMethodField()
     links = serializers.SerializerMethodField()
@@ -56,4 +30,30 @@ class VarSerializer(serializers.ModelSerializer):
         return {
             'self': drf_reverse('var-detail', kwargs={'pk': obj.pk}, request=request),
             'device': drf_reverse('device-detail', kwargs={'pk': obj.device.pk}, request=request),
+        }
+
+
+class DeviceSerializer(serializers.ModelSerializer):
+    vars = VarSerializer(many=True)
+    links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Device
+        read_only_fields = ('slug',)
+
+    def validate(self, data):
+        # TODO: This logic should reside in model save method
+        name_slug = slugify(data['name'])
+        data['slug'] = name_slug
+        i = 1
+        while Device.objects.filter(slug=data['slug']).count() > 0:
+            data['slug'] = '{}-{}'.format(name_slug, i)
+            i += 1
+        return data
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': drf_reverse('device-detail', kwargs={'pk': obj.pk}, request=request),
+            'vars': drf_reverse('var-list', request=request) + '?device={}'.format(obj.pk),
         }
