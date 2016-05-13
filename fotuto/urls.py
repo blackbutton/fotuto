7ed328a3594958a17064f3096132f8fa23990158
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-13 09:40:01 -0500

    Configure django to find and serve media, also ignore path in git

--- fotuto/urls.py
+++ fotuto/urls.py
@@ -1,5 +1,8 @@
 from importlib import import_module
+
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.utils.text import slugify
 from django.views.generic.base import TemplateView
@@ -38,7 +41,7 @@ urlpatterns = [
     url(r'^', include('vars.urls')),
     url(r'^', include('fmenus.urls')),
     url(r'^admin/', include(admin.site.urls)),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Dynamically add urls from extensions
 # TODO: Add a unittest for this
