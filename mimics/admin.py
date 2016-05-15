0adf6d4cbce9048362c6c6b502e938d0e017e69b
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-14 20:20:53 -0500

    ADMIN: Improve Mimics managements

--- mimics/admin.py
+++ mimics/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from mimics.models import Mimic, MimicVar, Rule
 
-admin.site.register(Mimic)
+
+class MimicVarInline(admin.TabularInline):
+    model = MimicVar
+
+
+class MimicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'window', 'x', 'y', 'width', 'height', 'graphic_type')
+    filter_horizontal = ('vars', 'rules')
+    inlines = (MimicVarInline,)
+
+admin.site.register(Mimic, MimicAdmin)
 admin.site.register(MimicVar)
 admin.site.register(Rule)
