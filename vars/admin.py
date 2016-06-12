f3b0b3f968b60e46cfde072871c581eb74f8fcfd
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-06-12 15:00:07 -0500

    ADMIN: Display device columns and filters

--- vars/admin.py
+++ vars/admin.py
@@ -5,5 +5,10 @@ from .models import Var, Device
 class VarAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'active', 'device', 'var_type', 'value', 'units')
 
-admin.site.register(Device)
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'active', 'model', 'address')
+    list_filter = ('active', 'model')
+
+admin.site.register(Device, DeviceAdmin)
 admin.site.register(Var, VarAdmin)
